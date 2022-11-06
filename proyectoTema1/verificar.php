46c9578e3c5aa4885f2730ba338a2baff98281de
sokas <aernestolh@gmail.com>
2022-11-06 13:55:52 +0100

    renamed:    proyectoTema1/login.php -> proyectoTema1/login.html 	new file:   proyectoTema1/verificar.php

---
+++ proyectoTema1/verificar.php
@@ -0,0 +1,34 @@
+<?php
+include './conexion/conexion.php';
+session_start();
+/* Si el usuario hace clic en entrar se obtiene
+el usuario y la contraseña */
+if(isset($_POST['entrar'])){
+  $user=$conecta->real_escape_string($_POST['usuario']);
+  $pass=$conecta->real_escape_string($_POST['contrasenia']);
+  
+  //Inyeccion SQL.
+  $consulta="SELECT * FROM usuario WHERE nick = '$user' and contrasenia = '$pass'";
+  
+  //Obtenemos los datos de la base de datos
+  if($resultado = $conecta->query($consulta)){
+    while($row = $resultado->fetch_array()){
+      $userCorrecto=$row['nick'];
+      $passCorrecto=$row['contrasenia'];
+
+    }
+    $resultado->close();
+  }
+  $conecta->close();
+  //Comprobamos que los datos son válidos
+  if(isset($user) && isset($pass)){
+    if($user==$userCorrecto && $pass==$passCorrecto){
+      $_SESSION['login']=TRUE;
+      $_SESSION['nick']=$usuario;
+      header("location:home.php");
+    }else{
+      echo "Los datos no son válidos";
+    }
+  }
+}
+?>
