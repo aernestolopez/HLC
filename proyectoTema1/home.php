4feadbf6bab5a7ed5bcaee2c89c74e767442f796
Sokas <99655174+aernestolopez@users.noreply.github.com>
2022-12-05 13:35:35 +0100

    Creacion de la paginacion

--- proyectoTema1/home.php
+++ proyectoTema1/home.php
@@ -9,6 +9,31 @@ $stmt->bindParam(":sesion", $id, PDO::PARAM_STR);
 $stmt->execute();
 $elresul = $stmt->fetch(PDO::FETCH_ASSOC);
 $nombre=$elresul['nick'];
+
+if(isset($_GET['page'])){
+  $page = $_GET['page'];
+}else{
+//Paginacion
+$page=1;
+}
+//Limite de datos por pagina
+$limit=40;
+$start = ($page - 1) * $limit;
+
+$next = $page + 1;
+$previous = $page - 1;
+//Obtenemos todos los datos
+$query = "SELECT * FROM paginacion LIMIT $start, $limit";
+$select = $conecta->prepare($query);
+$select->execute();
+$results = $select->fetchAll();
+
+//Obtenemos la cantidad de personas en la base de datos
+$query_count = "SELECT id FROM paginacion";
+$statement = $conecta->query($query_count);
+$total_datos = $statement->rowCount();
+//Calculamos la cantidad de paginas que vamos a necesitar
+$total_paginas = ceil($total_datos / $limit);
 ?>
 
 <!DOCTYPE html>
@@ -56,8 +81,38 @@ $nombre=$elresul['nick'];
     </ul>
   </div>
 </nav>
-  
-<h1>Si estas aqui es porque no la he liado
-    </h1>
+<!--Creamos una alerta por cada dato que exista-->
+    <?php foreach($results as $dato): ?>
+      <div class="alert alert-primary" role="alert">
+        <?php echo("ID: " . $dato['id']." Nombre: ". $dato['first_name']. " Apellido: ". $dato['last_name']. " Correo: " . $dato['email']. " Género: ". $dato['gender'])?>
+      </div>
+    <?php endforeach?>
+
+    <!--Creamos la navegacion-->
+    <nav aria-label="Page navigation">
+  <ul class="pagination justify-content-center">
+    <!--Mandamos al usuario a la primera pagina-->
+  <li class="page-item"><a class="page-link" href="home.php?page=1">Primera</a></li>
+  <!--Sie el usuario desea ir una pagina mas atras estando en la primera esta le redirigira a la primera-->
+    <li class="page-item"><a class="page-link" href="home.php?page=<?php echo $previous == 0 ? 1 : $previous ?>">Anterior</a></li>
+    <?php
+    //Hacemos que vayan desapareciendo paginas
+    for ($i = 1; $i <= $total_paginas; $i++) {
+      $current_page = $page;
+      $previous_2 = $current_page - 2;
+      $next_2 = $current_page + 2;
+
+      if ($i >= $previous_2 && $i<= $next_2) {
+    ?>
+    <!--Cuando estemos en una pagina el boton de esta se activara-->
+    <li class="page-item <?php echo $i == $page ? 'active' : '' ?>"><a class="page-link" href="home.php?page=<?php echo $i ?>"><?php echo $i ?></a></li>
+    <?php
+      }
+    }?>
+    <!--Si el usuario le da a siguiente estando en la ultima pagina esta le redirigirá a esta ultima-->
+    <li class="page-item"><a class="page-link" href="home.php?page=<?php echo $next > $total_paginas ? $total_paginas : $next ?>">Siguiente</a></li>
+    <!--Mandamos al usuario a la ultima pagina-->
+    <li class="page-item"><a class="page-link" href="home.php?page=<?php echo $total_paginas ?>">Última</a></li>
+  </nav>
 </body>
 </html>
